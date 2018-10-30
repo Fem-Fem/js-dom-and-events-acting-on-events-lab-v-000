5e78de228b94113dcf47c273c621e9f8e25bce89
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-30 17:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+// function preventRefreshOnSubmit(){
+//     document.querySelector('form').addEventListener('submit', function(event){
+//       event.preventDefault()
+//     })
+// }
+// 
+// preventRefreshOnSubmit()
+// 
+// let x = document.querySelector('input')
+// 
+// function retrieveEmployeeInformation() {
+//   return x.value
+// }
+// 
+// function addNewElementAsLi() {
+//   // return $('#baby-ninja');
+//   x = retrieveEmployeeInformation();
+//   document.querySelector('.employee-list').innerHTML = x;
+// }
+// 
+// // function addNewLiOnClick(){
+// //   let submit = document.querySelector('input[type="submit"]')
+// //   submit.addEventListener('click', function(event){
+// //     addNewElementAsLi()
+// //     // resetInput()
+// //   })
+// // }
+// 
+// function clearInput() {
+//   document.querySelector('input').value = ""
+// }
+// 
+// function addNewLiOnClick() {
+//   const main = document.querySelector('input[type="submit"]');
+//   main.addEventListener('click', function(event) {
+//     addNewElementAsLi();
+//     clearInput();
+//   });
+// }
+// 
+// function clearEmployeeListOnLinkClick() {
+//   const main = document.querySelector('a');
+//   const ul = document.querySelector('ul');
+//   main.addEventListener('click', function(event){
+//     ul.innerHTML = ""
+//   });
+// }
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
@@ -6,50 +54,34 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
-let x = document.querySelector('input')
+let input = document.querySelector('input')
 
-function retrieveEmployeeInformation() {
-  return x.value
+function retrieveEmployeeInformation(){
+  return input.value
 }
 
-function addNewElementAsLi() {
-  // return $('#baby-ninja');
-  x = retrieveEmployeeInformation();
-  document.querySelector('.employee-list').innerHTML = x;
+function addNewElementAsLi(){
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
 }
 
-// function addNewLiOnClick(){
-//   let submit = document.querySelector('input[type="submit"]')
-//   submit.addEventListener('click', function(event){
-//     addNewElementAsLi()
-//     // resetInput()
-//   })
-// }
 
-function clearInput() {
-  document.querySelector('input').value = ""
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi()
+    resetInput()
+  })
 }
 
-function addNewLiOnClick() {
-  const main = document.querySelector('input[type="submit"]');
-  main.addEventListener('click', function(event) {
-    addNewElementAsLi();
-    clearInput();
-  });
+function clearEmployeeListOnLinkClick(){
+  let link = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  link.addEventListener('click', function(event){
+    ul.innerHTML = ''
+  })
 }
 
-function clearEmployeeListOnLinkClick() {
-  const main = document.querySelector('a');
-  const ul = document.querySelector('ul');
-  main.addEventListener('click', function(event){
-    ul.innerHTML = ""
-  });
+function resetInput(){
+  document.querySelector('input').value = ''
 }
-
-// function clearEmployeeListOnLinkClick(){
-//   let link = document.querySelector('a')
-//   let ul = document.querySelector('ul')
-//   link.addEventListener('click', function(event){
-//     ul.innerHTML = ''
-//   })
-// }
