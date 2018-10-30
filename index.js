6fc208380c350e59360b8f8efa46bd802b4fafb9
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-30 16:49:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,20 @@ function retrieveEmployeeInformation() {
 // }
 
 function addNewElementAsLi() {
-  return $('#baby-ninja');
+  // return $('#baby-ninja');
+  document.querySelector('.employee-list').innerHTML).to.include('Bob')
+}
+
+function increaseRankBy(n) {
+  let array = document.querySelectorAll('ul.ranked-list')
+
+  for (let i = 0; i < array.length; i++) {
+    let this_child = array[i].children
+
+    for (let j = 0; j < this_child.length; j++) {
+      this_child[j].innerHTML = parseInt(this_child[j].innerHTML) + n
+    }
+  }
 }
 //
 // function addNewLiOnClick() {
